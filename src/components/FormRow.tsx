480bcb1d72c50f10d956b00fb6bbdf09c440531e
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2024-03-13 13:39:58 +0800

    style: improved form row styles

--- src/components/FormRow.tsx
+++ src/components/FormRow.tsx
@@ -1,20 +1,20 @@
 function FormRow(props) {
 
-  const { type, name, value, handleChange, labelText } = props
+    const {type, name, value, handleChange, labelText} = props
 
-  return (
-    <div className="form-row">
-      <label htmlFor={name} className="form-label">
-        {labelText ? labelText : name}
-      </label>
-      <input type={type}
-        id={name}
-        className="form-input"
-        value={value}
-        name={name}
-        onChange={handleChange} />
-    </div>
-  )
+    return (
+        <div className="form-row">
+            <label htmlFor={name} className="form-label">
+                {labelText ? labelText : name}
+            </label>
+            <input type={type}
+                id={name}
+                className="form-input"
+                value={value}
+                name={name}
+                onChange={handleChange}/>
+        </div>
+    )
 }
 
 export default FormRow
